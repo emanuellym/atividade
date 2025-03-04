9826dca696bb180dcc038f1b8389e0263d938dbf
EMANUELLY MACHADO SOARES <emanuelly_soares@estudante.sesisenai.org.br>
2025-03-04 16:16:52 -0300

    edita Ex9.java

--- Ex9.java
+++ Ex9.java
@@ -1,5 +1,11 @@
 package metodos;
 
 public class Ex9 {
-
+//teste de codigo
+	public static int conta(int num1,int num2, int num3, int num4) {
+		return num1+num2+num3+num4;
+	}
+	public static void main(String[]args) {
+		
+	}
 }
